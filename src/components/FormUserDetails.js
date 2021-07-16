4803573b3f36f4ccf9b02349d7fb7683a6ce9c84
boyangzhang1993 <boyangzhangosu@gmail.com>
2021-07-16 15:11:52 -0400

    Basically successfully made changes. Now all questions are in one page. Need to change css.

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -19,154 +19,81 @@ import Alert from 'react-bootstrap/Alert'
 export class DetailsForm extends Component {
 
 
-    continue = e => {
-        e.preventDefault();
-        console.log(this.props.values);
-        // checkForValue(this.props.values, '');
-
-        // test1 = this.props.values;
-      //   for(propT in this.props.values) {
-      //     if(this.props.values[propT] === "") { console.log('bad') }
-      // }
-        // if (this.props.values.test1_variable_number ==''){
-        //   <Alert variant='primary'>
-        //   Oops, please fill the table
-        // </Alert>}
-
-        if (this.props.values.test1_variable_number ==''){alert("Oops, something you forget to choose?")
-      }else if (this.props.values.test1_sample_size =='') {alert("Oops, something you forget to choose?")
-
-      }else if (this.props.values.test1_data_collection ==''){alert("Oops, something you forget to choose?")
-
-      }else if (this.props.values.test1_out_come ==''){alert("Oops, something you forget to choose?")
-
-      }else{
-
-      };
-
-        
-    }
-
-
-
-
 
     render() {
-        const{ values, handleChange } = this.props;
+        const{ values, handleChange, handleontinue } = this.props;
         // this.props.values;
         return (
-          <Container fluid>
-              <br />
-          <Container>
-        <Row>
-          <Col>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="https://picsum.photos/100/80" />
-            <Card.Body>
-              <Card.Title>Card Title 3</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-              <div>
-
-<>
-<br />
-    <Form.Group controlId="Form.ControlSelect3">
-      <h4>What is your outcome type?</h4>
-      <Form.Control as="select" onChange={handleChange('test1_out_come')}>
-        <option>Number</option>
-        <option>Category</option>
-        <option>Rank</option>
-      </Form.Control>
-    </Form.Group>
-      <Form>
-      <br />
-      <Form.Group controlId="Form.ControlSelect1">
-        <h4>How many variables do you have</h4>
-        <Form.Control as="select" onChange={handleChange('test1_variable_number')}>
-          <option>1-3</option>
-          <option>4-10</option>
-          <option>10-100</option>
-          <option>100+</option>
-        </Form.Control>
-      </Form.Group>
-      <br />
-      <Form.Group controlId="Form.ControlSelect2">
-        <h4>How many sample size do you have</h4>
-        <Form.Control as="select" onChange={handleChange('test1_sample_size') }>
-          <option>1-10</option>
-          <option>10-20</option>
-          <option>20-50</option>
-          <option>50-100</option>
-          <option>100+</option>
-        </Form.Control>
 
 
-      </Form.Group>
-      
-
-    <br />
-    <Form.Group controlId="Form.ControlSelect4">
-      <h4>How did you collect data?</h4>
-      <Form.Control as="select" onChange={handleChange('test1_data_collection') }>
-        <option>Data were collected from a Experiment</option>
-        <option>Data were collected from observersions such as survey.</option>
-        <option>Not sure</option>
-
-      </Form.Control>
-    </Form.Group>
-    
-  
-  <br />                  
-    </Form>
-    <br />
-    <Button
-      variant="primary"
-      onClick={this.continue}
-    >Continue</Button>
-
-</>
-
-</div>
-            </Card.Body>
-            </Card>
-          </Col>
-            <Col>
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="https://picsum.photos/100/80" />
+            <Card style={{ width: '28rem' }}>
+              {/* <Card.Img variant="top" src="https://picsum.photos/100/80" /> */}
               <Card.Body>
-                <Card.Title>Card Title 3</Card.Title>
-                <Card.Text>
+                <Card.Title>Let's learn about your data</Card.Title>
+                {/* <Card.Text>
                   Some quick example text to build on the card title and make up the bulk of
                   the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                </Card.Text> */}
+              <div>
+                <>
+                <br />
+                <Form.Group controlId="Form.ControlSelect3">
+                  <h4>What is your outcome type?</h4>
+                  <Form.Control as="select" onChange={handleChange('test1_out_come')}>
+                    <option>Number</option>
+                    <option>Category</option>
+                    <option>Rank</option>
+                  </Form.Control>
+                </Form.Group>
+                <Form>
+                <br />
+                <Form.Group controlId="Form.ControlSelect1">
+                  <h4>How many variables do you have</h4>
+                  <Form.Control as="select" onChange={handleChange('test1_variable_number')}>
+                    <option>1-3</option>
+                    <option>4-10</option>
+                    <option>10-100</option>
+                    <option>100+</option>
+                  </Form.Control>
+                </Form.Group>
+                <br />
+                <Form.Group controlId="Form.ControlSelect2">
+                  <h4>How many sample size do you have</h4>
+                  <Form.Control as="select" onChange={handleChange('test1_sample_size') }>
+                    <option>1-10</option>
+                    <option>10-20</option>
+                    <option>20-50</option>
+                    <option>50-100</option>
+                    <option>100+</option>
+                  </Form.Control>
+
+                </Form.Group>
+                
+              <br />
+              <Form.Group controlId="Form.ControlSelect4">
+                <h4>How did you collect data?</h4>
+                <Form.Control as="select" onChange={handleChange('test1_data_collection') }>
+                  <option>Data were collected from a Experiment</option>
+                  <option>Data were collected from observersions such as survey.</option>
+                  <option>Not sure</option>
+
+                </Form.Control>
+              </Form.Group>
+            <br />                  
+              </Form>
+              <br />
+              <Button
+                variant="primary"
+                onClick={handleontinue}
+              >Continue</Button>
+                    </>
+                    </div>
               </Card.Body>
               </Card>
 
 
-            </Col>
-              <Col>
-              <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="https://picsum.photos/100/80" />
-                <Card.Body>
-                  <Card.Title>Card Title 3</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                  </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-                </Card>
-              </Col>
-      </Row>
-
-      </Container>  
-
-
           
-          </Container>
+          
 
 
 
@@ -176,3 +103,6 @@ export class DetailsForm extends Component {
 }
 
 export default DetailsForm
+
+
+
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,97 +1,193 @@
 import React, { Component } from 'react'
 import DetailsForm from "./FormUserDetails"
-import Form2 from "./FormPersonalDetails"
+import Form from 'react-bootstrap/Form'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+
 
 export class UserForm extends Component {
     state = {
         test1_variable_number:'Number',
         test1_sample_size:'1-3',
         test1_data_collection:'1-10',
-        test1_out_come:'Data were collected from a Experiment'
+        test1_out_come:'Data were collected from a Experiment',
+        isLoggedIn: false
     
     }
-    // Proceed to next steps
-    nextStep = () =>{
-        const { step } = this.state;
-        this.setState({
-            step: step + 1
-        });
 
+    // Handle Continue button
+    handleontinue = e => {
+        e.preventDefault();
+        console.log(this.state);
+        if (this.state.test1_variable_number ==''){alert("Oops, something you forget to choose?")
+      }else if (this.state.test1_sample_size =='') {alert("Oops, something you forget to choose?")
 
+      }else if (this.state.test1_data_collection ==''){alert("Oops, something you forget to choose?")
+
+      }else if (this.state.test1_out_come ==''){alert("Oops, something you forget to choose?")
+
+      }else{
+
+      };
+      this.setState({isLoggedIn: true});
+
+        
     }
-    //Go back to prev steps
-    prevStep = () =>{
-        const { step } = this.state;
-        this.setState({
-            step: step - 1
-        });
 
-
-    }
     // Handle fields change
     handleChange = input => e => {
         this.setState({[input]:e.target.value});
         console.log(this.state);
+        
 
     }
+    // Handle Continue button
+    handleLoginClick() {
+        this.setState({isLoggedIn: true});
+      }
+    
+      handleLogoutClick() {
+        this.setState({isLoggedIn: false});
+      }
 
     render() {
         const { step } = this.state;
         const { test1_variable_number,test1_sample_size,test1_data_collection,test1_out_come } = this.state;
         const values = { test1_variable_number,test1_sample_size,test1_data_collection,test1_out_come }
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+        if (isLoggedIn) { button = <LogoutButton onClick={this.handleLogoutClick} />;    
+        } else {button = <LoginButton onClick={this.handleLoginClick} />; }
+
+
         return(
             <div>
-            <DetailsForm 
-                nextStep = {this.nextStep}
-                handleChange = {this.handleChange}
-                values = {values}
-            />
+                <Greeting isLoggedIn={isLoggedIn} />        
+                
+
+
+                <Row>
+                <Col>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="https://picsum.photos/100/80" />
+                        <Card.Body>
+                        <Card.Title> Instruction </Card.Title>
+                        <Card.Text>
+                        <NewlineText text={'1. Fill the form on the left.\n2. Press Continue button.\n3. Select a recommended procedure.'} />
+                                {/* 1. Fill the form on the left.
+                                2. Press Continue button.
+                                3. Select a recommended procedure. */}
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <DetailsForm 
+                    handleChange = {this.handleChange}
+                    values = {values}
+                    handleontinue = {this.handleontinue}
+                    />
+                </Col>
+                <Col>
+                <ThirdCol isLoggedIn={isLoggedIn} />    
+
+                </Col>
+
+                
+
+                    </Row>
+
+
+
             </div>
 
         )
 
-        // switch(step){
-        //     case 1:
-        //         return(
-        //             <div>
-        //             <DetailsForm 
-        //                 nextStep = {this.nextStep}
-        //                 handleChange = {this.handleChange}
-        //                 values = {values}
-        //             />
-        //             <h1>Case 1</h1>
-
-        //             </div>
-
-        //         )
-        //     case 2:
-        //         return(
-        //             <div>
-        //             <Form2 
-        //                 nextStep = {this.nextStep}
-        //                 handleChange = {this.handleChange}
-        //                 values = {values}
-        //                 prevStep={this.prevStep}
-        //             />
-        //             <h1>Case 2</h1>
-
-        //             </div>
-        //         )
-        //     case 3:
-        //         return(
-        //             <h1>Confirm</h1>)
-        //     case 4:
-        //         return(
-        //             <h1>4</h1>
-        //         )
-
-        // }
-        // return (
-        //     <div>
-                
-        //     </div>
-        // )
     }
 }
 
 export default UserForm
+
+
+
+function NewlineText(props) {
+    const text = props.text;
+    const newText = text.split('\n').map(str => <p>{str}</p>);
+    
+    return newText;
+  }
+
+
+  function UserGreeting(props) {
+    return (
+            <div>
+            <h1>This is what we recommend for you. </h1>
+            <p>
+            You can redo the survey by prcess continue button.
+          </p>
+            <hr/>
+            </div>);
+  }
+  
+  function GuestGreeting(props) {
+    return (
+        <div >
+            <h1>Welcome to Stat Pillow</h1>
+            <p>
+            Fill the formin the center> Prcess continue > Select a procedure
+          </p>
+          <hr/>
+        </div>
+    );
+  }
+  function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) { return <UserGreeting />;}return <GuestGreeting />;}
+    function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+        Login
+        </button>
+    );
+    }
+
+    function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+        Logout
+        </button>
+    );
+    }
+
+
+      function ThirdCol(props) {
+        const isLoggedIn = props.isLoggedIn;
+        if (isLoggedIn) { return <RecommendedTest />;}return <DefaultResult />;}
+
+        function RecommendedTest(props) {
+            return (
+                    <div>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="https://picsum.photos/100/80" />
+                        <Card.Body>
+                        <Card.Title> Instruction </Card.Title>
+                        <Card.Text>
+                        <NewlineText text={'1. Fill the form on the left.\n2. Press Continue button.\n3. Select a recommended procedure.'} />
+                                {/* 1. Fill the form on the left.
+                                2. Press Continue button.
+                                3. Select a recommended procedure. */}
+                        </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    </div>);
+          }
+          
+          function DefaultResult(props) {
+            return (
+                <div >
+
+                </div>
+            );
+          }
